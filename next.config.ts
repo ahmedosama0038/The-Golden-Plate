db7cfea6da7843a1eadaba2713b44917ceed0c04
ahmedosama0038 <ah.osama303@gmail.com>
2026-06-02 09:46:26 +0300

    fix: disable turbopack for production build

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    // !! WARNING !!
-    // بالتعديل ده بنقول لـ Next.js كملي الـ Build حتى لو فيه إيرور في الـ TypeScript
-    ignoreBuildErrors: true,
-  },
   images: {
     remotePatterns: [
       {
