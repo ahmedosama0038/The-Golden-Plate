321320f4dd369daba871f5c3a6e750def8c1b455
ahmedosama0038 <ah.osama303@gmail.com>
2026-06-02 09:50:38 +0300

    fix: restore build script and add standalone output

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone', // 👈 ضيف السطر ده هنا
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     remotePatterns: [
       {
